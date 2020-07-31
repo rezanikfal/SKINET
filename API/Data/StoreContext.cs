587a9b7277c2f752e7b7d8ea0df2db5997d3175c
Reza Nikfal <mr.nikfal@gmail.com>
2020-07-30 21:10:31 -0500

    Setup connection string for SQlite

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -5,8 +5,9 @@ namespace API.Data
 {
     public class StoreContext : DbContext
     {
-        public StoreContext( DbContextOptions<StoreContext> options) : base(options)
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
         {
         }
         public DbSet<Product> Products { get; set; }
+    }
 }
