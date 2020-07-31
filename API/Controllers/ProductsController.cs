3fbfa5c031212ec2e0891a45aa01974a1922a3dd
Reza Nikfal <mr.nikfal@gmail.com>
2020-07-30 21:34:28 -0500

    Get Products Endpoint

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,10 +10,17 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
         {
-            return "This is the list of products";
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products);
         }
         [HttpGet("{id}")]
         public string GetProduct(int id)
